d2b7418f6a0a1d59bf00e63fbc5a613ad6226e23
Yuli Zhong <yz924@cornell.edu>
2016-11-01 13:41:11 -0700

    implement discription

--- src/components/move_description.js
+++ src/components/move_description.js
@@ -5,21 +5,23 @@ export default class MoveDescription extends Component {
   componentWillMount() {
     this.props.getMove(this.props.pokemon.moves.move_1);
   }
-  renderMove() {
+  renderMove(move) {
     return(
       <ListItem>
-        <h2>Move: {this.props.moveDetail.move_1.name}</h2>
-        <h4>Power: {this.props.moveDetail.move_1.power}</h4>
-        <p>{this.props.moveDetail.move_1.flavor_text_entries[1].flavor_text}</p>
+        <h2>Move: {move.name}</h2>
+        <h4>Power: {move.power}</h4>
+        <p>{move.flavor_text_entries[1].flavor_text}</p>
       </ListItem>
     )
   }
   render() {
+    const { move_1, move_2 } = this.props.moveDetail;
     return(
       <div className="move_discription">
         <h1>Move description</h1>
         <List className="move_discrip_detail">
-          {this.props.moveDetail.move_1?this.renderMove():<h1>No move</h1>}
+          {this.props.moveDetail.move_1?this.renderMove(move_1):<h1>Move 1</h1>}
+          {this.props.moveDetail.move_2?this.renderMove(move_2):<ListItem><h3>Not Learnt</h3></ListItem>}
         </List>
       </div>
     )
